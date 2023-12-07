2c6e843438d2f35091aff2e635f355a17685347d
chalscc <carlescatalancalabuig@gmail.com>
2023-12-07 19:41:45 +0100

    Componente de busqueda

--- src/heroes/pages/SearchPage.jsx
+++ src/heroes/pages/SearchPage.jsx
@@ -1,7 +1,47 @@
+import { HeroCard } from "../components"
+
 export const SearchPage = () => {
   return (
     <>
-        <h1>SearchPage</h1>
+      <h1>SearchPage</h1>
+      <hr />
+      <div className="row">
+        <div className="col-5">
+          <h4>Searching</h4>
+          <hr />
+
+          <form action="">
+
+            <input
+              type="text"
+              placeholder="Search a hero"
+              className="form-control"
+              name="searchText"
+              autoComplete="off"
+            />
+            <button
+              className="btn mt-2 btn-outline-primary">
+              Search
+            </button>
+          </form>
+        </div>
+
+        <div className="col-7">
+          <h4>Results</h4>
+          <hr />
+
+          <div className="alert alert-primary">
+            Search a hero
+          </div>
+
+          <div className="alert alert-danger">
+            There is no results
+          </div>
+
+          {/* <HeroCard/> */}
+
+        </div>
+      </div>
     </>
   )
 }
