e4605c0d172b4a48ea297940c6801c174f23ef6c
chalscc <carlescatalancalabuig@gmail.com>
2023-12-10 14:11:12 +0100

    Rutas publicas

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,23 +1,34 @@
 import { Navigate, Route, Routes } from "react-router-dom"
 
 import { Navbar } from "../ui"
-import { LoginPage }  from "../auth"
+import { LoginPage } from "../auth"
 import { DCPage, HeroesRoutes, MarvelPage } from "../heroes"
 import { PrivateRoute } from "../heroes/routes/PrivateRoute"
+import { PublicRoute } from "../heroes/routes/PublicRoute"
 
 export const AppRouter = () => {
   return (
     <>
-        <Routes>
-            <Route path="login" element={<LoginPage />} />
+      <Routes>
+        <Route path="login" element={
+          
+          // Si el usuario ya esta logeado, lo redirecciona a la pagina principal
+          // en el caso que quiera ir a /login, para ir a /login, debe deslogearse
+          <PublicRoute>
+            <LoginPage />
+          </PublicRoute>
+        } />
 
-            <Route path="/*" element={
-              <PrivateRoute>
-                <HeroesRoutes />
-              </PrivateRoute>
-            } />
+        <Route path="/*" element={
 
-        </Routes>
+          //  Si el usuario no esta logeado, lo redirecciona a la pagina de login
+          //  No le permitira acceder a ninguna otra pagina
+          <PrivateRoute>
+            <HeroesRoutes />
+          </PrivateRoute>
+        } />
+
+      </Routes>
     </>
   )
 }
